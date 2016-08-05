4a8f8e7c8b41acbc90ecbdbf4c25621731878a9b
CzlowiekImadlo <quak@tlen.pl>
2016-08-05 22:25:53 +0200

    Added quit button

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -1,4 +1,17 @@
 extends "res://scripts/services/abstract_screen.gd"
 
+var play_button
+var quit_button
+
 func _init():
     self.screen_scene = preload("res://scenes/menu/main.tscn").instance()
+    self.bind()
+
+func bind():
+    self.play_button = self.screen_scene.get_node('play_button')
+    self.quit_button = self.screen_scene.get_node('quit_button')
+
+    self.quit_button.connect('pressed', self, '_quit_button_pressed')
+
+func _quit_button_pressed():
+    OS.get_main_loop().quit()
