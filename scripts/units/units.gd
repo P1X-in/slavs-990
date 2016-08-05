d978c62898b4c2c78f0dff86c83510056f2306dc
WermooZ <p.weremczuk@gmail.com>
2016-08-06 00:57:19 +0200

    random damage value

--- scripts/units/units.gd
+++ scripts/units/units.gd
@@ -3,16 +3,16 @@ var units = [
         'name': 'szczur',
         'family' : 'szczury',
         'description': 'małe i gryzie',
-        'hp': 1,
+        'hp': 10,
         'ap': 5,
         'resistances' : {
-            'normal' : {'threshold' : 0, 'resistance': 0.05}
+            'normal' : {'threshold' : 0, 'resistance': 0.10}
             # 'slashing'
             # 'bludgeoning'
             # 'piercing'
         },
         'attacks' : {
-            'bite' : {'damage' : 1, 'skill' : 0.20, 'critical': 0.01}
+            'bite' : {'damage' : 4, 'skill' : 0.20, 'critical': 0.02}
         },
         'challenge' : 1,
         'xp' : 10,
