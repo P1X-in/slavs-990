223e1a485c63c3da2a05f07e267f2d5bb5f99721
Krzysztof Jankowski <w84death@gmail.com>
2016-08-07 00:29:01 +0200

    better resources panel

--- scripts/controllers/hud_controller.gd
+++ scripts/controllers/hud_controller.gd
@@ -7,10 +7,10 @@ func refresh_resources():
     var resources_panel = self.bag.board.screen_scene.get_node('resources_panel/list')
     var resources = self.bag.resources.resources
 
-    resources_panel.get_node('drewno').set_text('drewno: '+ str(resources['wood']))
-    resources_panel.get_node('mieso').set_text('mięso: '+ str(resources['meat']))
-    resources_panel.get_node('zborze').set_text('zborze: '+ str(resources['wheat']))
-    resources_panel.get_node('metal').set_text('metal: '+ str(resources['metal']))
-    resources_panel.get_node('bursztyn').set_text('bursztyn: '+ str(resources['amber']))
+    resources_panel.get_node('drewno/box/value').set_text(str(resources['wood']))
+    resources_panel.get_node('mieso/box/value').set_text(str(resources['meat']))
+    resources_panel.get_node('zborze/box/value').set_text(str(resources['wheat']))
+    resources_panel.get_node('metal/box/value').set_text(str(resources['metal']))
+    resources_panel.get_node('bursztyn/box/value').set_text(str(resources['amber']))
 
 
