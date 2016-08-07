693770037c5b18c77507715163fbd834aa64e054
CzlowiekImadlo <quak@tlen.pl>
2016-08-07 11:29:55 +0200

    Fixed enemy names in inspect

--- scripts/services/inspect.gd
+++ scripts/services/inspect.gd
@@ -2,6 +2,8 @@ extends "res://scripts/services/abstract_screen.gd"
 
 var exit_button
 
+var name_label
+
 var wolf_avatar
 var wolf_description
 
@@ -18,6 +20,8 @@ func _init():
 func bind():
     self.exit_button = self.screen_scene.get_node('panel/container/footer/exit_button')
     self.exit_button.connect('pressed', self, '_exit_button_pressed')
+
+    self.name_label = self.screen_scene.get_node('panel/container/header/Label')
 
     self.wolf_avatar = self.screen_scene.get_node('panel/container/body/info/avatars/wilk')
     self.cmuch_avatar = self.screen_scene.get_node('panel/container/body/info/avatars/cmuch')
@@ -44,9 +48,12 @@ func inspect_event(event):
     if event.enemy_icon_type == self.bag.abstract_map.TILE_FOREST_ANIMAL:
         self.wolf_avatar.show()
         self.wolf_description.show()
+        self.name_label.set_text('WILKI')
     if event.enemy_icon_type == self.bag.abstract_map.TILE_WATER_CREATURE:
         self.cmuch_avatar.show()
         self.cmuch_description.show()
+        self.name_label.set_text('ĆMOCH')
     if event.enemy_icon_type == self.bag.abstract_map.TILE_FOREST_CREATURE:
         self.borowy_avatar.show()
         self.borowy_description.show()
+        self.name_label.set_text('BOROWY')
